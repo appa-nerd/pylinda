b96a17608bbaea8ec5aee5cc074b7c78dda90467
appa <you@example.com>
2016-08-22 00:45:52 -0600

    test

--- linda_server.py
+++ linda_server.py
@@ -109,7 +109,8 @@ class server(object):
 
     def recv(self,sock):
         _buffer_ = sock.recv(4)
-        _recv_buff = int(struct.unpack('!I', _buffer_))
+        _recv_buff = struct.unpack('!I', _buffer_)
+        print _recv_buff
         data = sock.recv(_recv_buff)
         return pickle.loads(data)
 
